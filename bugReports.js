8282abd37772eecb8520f9730df74540d6e6b16f
tx-ro <matiaspersonal06@gmail.com>
2025-08-17 16:33:00 -0300

    Update bugReports.js

--- bugReports.js
+++ bugReports.js
@@ -14,7 +14,7 @@ if (bugInput) {
       };
 
       try {
-        const res = await fetch('https://backend-website-h19f.onrender.com/report', { 
+        const res = await fetch('https://backend-website-h19f.onrender.com/', { 
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(payload)
